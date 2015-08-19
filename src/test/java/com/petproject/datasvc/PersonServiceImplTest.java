5012808c617e3e04da4e635d177f9b36bfd8f282
pekarsky <idlev100>
2015-08-19 10:45:59 +0200

    person service test stub

---
+++ src/test/java/com/petproject/datasvc/PersonServiceImplTest.java
@@ -0,0 +1,35 @@
+package com.petproject.datasvc;
+
+import junit.framework.TestCase;
+
+
+public class PersonServiceImplTest extends TestCase {
+
+    public void testSave() throws Exception {
+
+    }
+
+    public void testDelete() throws Exception {
+
+    }
+
+    public void testUpdate() throws Exception {
+
+    }
+
+    public void testGetPersonById() throws Exception {
+
+    }
+
+    public void testGetAllPersons() throws Exception {
+
+    }
+
+    public void testGetAllGroups() throws Exception {
+
+    }
+
+    public void testAddGroupToPerson() throws Exception {
+
+    }
+}
