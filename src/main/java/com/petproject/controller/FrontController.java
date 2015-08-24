b8e8dab0dce0e4b6707e46bb7136bb4a4ec739d8
pekarsky <idlev100>
2015-08-24 14:03:33 +0200

    deployment to tomcat

---
+++ src/main/java/com/petproject/controller/FrontController.java
@@ -0,0 +1,30 @@
+package com.petproject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class FrontController {
+
+//    @RequestMapping(method = RequestMethod.GET)
+ //   @ResponseBody
+  //  public String default(){
+   //     System.out.println("sdgsdgsdfgsdfg");
+        //return "<br><div style='text-alig:center;'><h3>********** Hello World, Spring MVC Tutorial</h3>This message is coming from Fron";
+        //return new ModelAndView("welcome", "message", message);
+    //}
+
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ModelAndView helloWorld() {
+
+        String message = "<br><div style='text-align:center;'>"
+                + "<h3>********** Hello World, Spring MVC Tutorial</h3>This message is coming from FronController.java **********</div><br><br>";
+        return new ModelAndView("welcome", "message", message);
+    }
+
+
+}
