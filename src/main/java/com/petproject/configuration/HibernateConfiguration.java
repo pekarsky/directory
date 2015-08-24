302189b233548009ff04b51b1abcc01ce4d39fca
pekarsky <idlev100>
2015-08-24 10:37:53 +0200

    changes in hibernate comfig

--- src/main/java/com/petproject/configuration/HibernateConfiguration.java
+++ src/main/java/com/petproject/configuration/HibernateConfiguration.java
@@ -49,7 +49,9 @@ public class HibernateConfiguration {
         properties.put("hibernate.dialect", environment.getRequiredProperty("hibernate.dialect"));
         properties.put("hibernate.show_sql", environment.getRequiredProperty("hibernate.show_sql"));
         properties.put("hibernate.format_sql", environment.getRequiredProperty("hibernate.format_sql"));
-        properties.put("hibernate.hbm2ddl.auto", environment.getRequiredProperty("hibernate.hbm2ddl.auto"));
+        if(environment.containsProperty("hibernate.hbm2ddl.auto")) {
+            properties.put("hibernate.hbm2ddl.auto", environment.getProperty("hibernate.hbm2ddl.auto"));
+        }
         return properties;
     }
 
